3e0dffdbeef32c1e383fa4d0484ecf027c733a16
GBatriche <batricheg@gmail.com>
2024-09-24 10:10:21 -0500

    Exercicio

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+document.getElementById('validacao').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const num1 = parseFloat(document.getElementById('numero1').value);
+    const num2 = parseFloat(document.getElementById('numero2').value);
+    const resultado = document.getElementById('resultado');
+    const erro = document.getElementById('erro');
+
+    resultado.textContent = '';
+    erro.textContent = '';
+
+    if (num2 > num1) {
+
+        resultado.textContent = 'Fomulário validado!';
+    } else {
+        erro.textContent = 'Erro: O segundo número deve ser maior que o primeiro.';
+    }
+});
